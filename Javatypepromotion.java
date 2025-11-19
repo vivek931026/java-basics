8f065c662b958a27d4252e2cb7bd7b05b0cb3824
vivek931026 <vivek931026@gmail.com>
2025-11-20 02:54:12 +0530

    Add files via upload

---
+++ Javatypepromotion.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Javatypepromotion {
+    public static void main(String ergs[]){
+        // type promotion in expression
+        char a = 'a';
+        char b = 'b';
+        System.out.println((int) a);
+        System.out.println((int) b);
+        System.out.println(b - a);
+
+
+        int c = 10;
+        float d = 20.25f;
+        long e = 25;
+        double f = 30;
+        double ans = c + d + e +f;
+        System.out.println(ans);
+    }
+}
+
