93e4723f544b2e80f45ec59d504b8a7adeb30577
vivek <vivek931026@gmail.com>
2025-11-21 23:26:17 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,17 @@
+
+public class ifelse {
+    public static void main(String args[]) {
+    // Print the largest of 2 number
+    
+    int A = 5;
+    int B = 852;
+    
+    if (A >= B){
+        System.out.println("A is the largest of 2");
+    }else{
+        System.out.println("B is the largest of 2");
+    }
+
+
+    }
+}
