2c081db40c9544640d68a2f1ede4f9759121da4b
vivek <vivek931026@gmail.com>
2025-11-23 00:25:52 +0530

    Update ifelse.java

--- ifelse.java
+++ ifelse.java
@@ -1,17 +1,36 @@
-
-public class ifelse {
+import java.util.*;
+public class ifelse2 {
     public static void main(String args[]) {
-    // Print the largest of 2 number
     
-    int A = 5;
-    int B = 852;
-    
-    if (A >= B){
-        System.out.println("A is the largest of 2");
-    }else{
-        System.out.println("B is the largest of 2");
-    }
+    int num = 4;
 
+    // ternary operator 
+    String type = ((num%2) == 0) ? "Even" : "Odd";
+    System.out.println(type);
+        
+    // check if a student will pass or fail 
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter marks: ");
+    int marks = sc.nextInt();
+    
+    String result = (33 < marks) ? "pass" : "fail";
+    System.out.println(result);
+
+    // Switch statement 
+
+    int number = 2;
+    switch(number){
+        case 1 : System.out.println("samosa");
+                    break;
+        case 2 : System.out.println("burger");
+                    break;
+        case 3 : System.out.println("mango shake");
+                    break;
+        default : System.out.println("we wake up");
+    }
 
     }
 }
+
