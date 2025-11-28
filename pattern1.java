78e659bc0a5c4e644bbe45bae4d100d538bee435
vivek <vivek931026@gmail.com>
2025-11-28 23:34:04 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,30 @@
+public class pattern1{
+    public static void main(String args[]){
+        // print star pattern
+        // *
+        // **
+        // ***
+        // ****
+        for (int row = 1; row <= 4; row++){
+            for (int col = 1;  col <= row; col++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // print invented-star pattern
+        // ****
+        // ***
+        // **
+        // *
+        for (int row = 1; row <= 4; row++){
+            for (int col = 1;  col <= row; col++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
