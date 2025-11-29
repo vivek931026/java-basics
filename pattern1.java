80622424cfe6dddd4cb142e9865e92eb282a1873
vivek <vivek931026@gmail.com>
2025-11-29 23:47:53 +0530

    Refactor star pattern printing in pattern1.java
    Added a newline between the two star patterns and corrected the loop for the inverted star pattern.

--- pattern1.java
+++ pattern1.java
@@ -11,14 +11,15 @@ public class pattern1{
             }
             System.out.println();
         }
-
+        System.out.println();
         // print invented-star pattern
         // ****
         // ***
         // **
         // *
-        for (int row = 1; row <= 4; row++){
-            for (int col = 1;  col <= row; col++ ){
+        int n = 4;
+        for (int row = 1; row <= n; row++ ){
+            for (int col = 1;  col <=n - row + 1; col++ ){
                 System.out.print("*");
             }
             System.out.println();
