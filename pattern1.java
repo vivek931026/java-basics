697e0537611853fbf1b65cfd0cc3431d0e038dd2
vivek <vivek931026@gmail.com>
2026-01-08 23:00:47 +0530

    Enhance pattern1.java with additional patterns
    Added half-pyramid and character patterns to the existing star patterns.

--- pattern1.java
+++ pattern1.java
@@ -25,7 +25,36 @@ public class pattern1{
             System.out.println();
         }
 
+        // half-pyramid pattern
+        // 1
+        // 12
+        // 123
+        // 1234
+        // 12345
+        // for (int row=1; row<=5; row++){
+        //     for(int col=1; col<=row; col++){
+        //         System.out.print(col);
+        //     }
+        //     System.out.println();
+        // }
+
+        // character pattern
+        // A
+        // BC
+        // DEF
+        // GJIJ
+
+        char ch = 'A';
+
+        for (int row=1; row<=4; row++){
+            for(int col=1; col<=row; col++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
 
 
     }
 }
+
