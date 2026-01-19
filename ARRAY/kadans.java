a66c7e6030dc7da2f7362b1f8ddad81981aa47aa
vivek <vivek931026@gmail.com>
2026-01-19 23:56:39 +0530

    Add files via upload

---
+++ ARRAY/kadans.java
@@ -0,0 +1,20 @@
+public class kadans {
+
+    public static void kadansAlgo(int num[]){
+        int cs=0;
+        int ms=Integer.MIN_VALUE;
+        for(int i=0; i<num.length; i++){
+            cs = cs + num[i];
+            if(cs < 0){
+                cs = 0;
+                ms = Math.max(cs, ms);
+            }
+        }
+        System.out.println("our maximum sum of subarray is: "+ms);
+    }
+
+    public static void main(String[] args){
+        int num[] = {-2,-3,4,-1,-2,1,5,-3};
+        kadansAlgo(num);
+    }
+}
