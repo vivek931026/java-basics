1f6883cc8688edc2942b47c26700d2a2f5ae6e63
vivek <vivek931026@gmail.com>
2026-02-01 22:22:56 +0530

    Add files via upload

---
+++ 2D array/matrices.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class matrices{
+
+    public static boolean searchMatrix(int matrix[][], int key){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == key){
+                System.out.println("found at cell ("+ i + "," + j + ")");
+                return true;
+                }
+            }
+        }
+        System.out.println("key not found");
+        return false;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int matrix[][] = new int[3][3];
+        int n=matrix.length, m=matrix[0].length; // for row = matrix.length, for column = matrix[0].length
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        searchMatrix(matrix, 6);
+
+        sc.close();
+    }
+}
